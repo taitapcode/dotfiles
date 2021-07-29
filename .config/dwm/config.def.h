82780cc80011f722152f4abe19677476ce49ffdf
TaiK7 <hoangductai2007@gmail.com>
2021-07-29 09:34:08 +0700

    Add copy config script

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -19,7 +19,7 @@ static const int swallowfloating    = 1;        /* 1 means swallow floating wind
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "FiraCode Nerd Font:size=12:antialias=true:autohint=true" };
-static const char col_gray1[]       = "#020814";
+static const char col_gray1[]       = "#0c1424";
 static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
