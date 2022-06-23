6e392b44c99eed5404536c159ace2ce8367f89f2
noname0204 <hoangductai2007@gmail.com>
2022-06-23 16:35:43 -0700

    update installer

--- installer_for_arch_wsl.sh
+++ installer_for_arch_wsl.sh
@@ -22,17 +22,17 @@ paru -c
 echo "Set default shell to zsh..."
 chsh -s $(which zsh)
 
-# Clone config
-echo "Clone config repo..."
-git clone --depth=1 https://github.com/noname0203/dotfiles -b wsl ~/.dotfiles
-cp ~/.dotfiles/.config ~ -r
-cp ~/.dotfiles/.zsh ~ -r
-
 # Install Oh My Zsh
 echo "Install Oh-My-Zsh..."
 export ZDOTDIR="$HOME/.zsh"
 zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Clone config
+echo "Clone config repo..."
+git clone --depth=1 https://github.com/noname0203/dotfiles -b wsl ~/.dotfiles
+cp ~/.dotfiles/.config ~ -r
+cp ~/.dotfiles/.zsh/* ~ -r
 
 # Install zsh plugins
 echo "Install zsh-vi-mode, zsh-autosuggestions, fast-syntax-highlighting..."
@@ -49,6 +49,7 @@ echo "Login to github..."
 gh auth login
 
 cd
+echo "Make sure you use Nerd Font for icon: https://www.nerdfonts.com/font-downloads"
 echo "Before reset, make sure you have 'export ZDOTDIR=\$HOME/.zsh' in '/etc/zsh/zshenv'"
 
 exit 0
