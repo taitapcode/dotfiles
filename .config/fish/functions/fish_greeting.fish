7944d118197522f39202c68a8ec20cce2e6f82a4
noname0204 <hoangductai2007@gmail.com>
2022-06-26 17:37:32 +0100

    update fish_greeting function

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,17 @@
 function fish_greeting
-    echo Wellcome back! Today is (set_color yellow; date "+%d/%m/%Y")
+  set -f normal (set_color normal)
+  set -l cyan (set_color -o cyan)
+  set -l green (set_color -o green)
+  set -f h (date +%H)
+  set -f date (date +%d/%m/%Y)
+  if [ $h -lt 12 ]
+    set -f wellcome 'Good Morning!'
+  else if [ $h -lt 18 ]
+    set -f wellcome 'Good Affternoon!'
+  else if [ $h -lt 21 ]
+    set -f wellcome 'Good Evening!'
+  else
+    set -f wellcome 'Good Night!'
+  end
+  echo $cyan$wellcome$normal Today is date $green$date
 end
