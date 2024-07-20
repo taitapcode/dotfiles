3120040475254224225cdb74f64d8de79036c6e8
--global <--global>
2024-07-20 23:51:47 +0700

    add Ibus bamboo autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,30 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Ibus bamboo
+vim.cmd([[
+function! IBusOff()
+  " Lưu engine hiện tại
+  let g:ibus_prev_engine = system('ibus engine')
+  " Chuyển sang engine tiếng Anh
+  execute 'silent !ibus engine BambooUs'
+endfunction
+function! IBusOn()
+  let l:current_engine = system('ibus engine')
+  " nếu engine được set trong normal mode thì
+  " lúc vào insert mode duùn luôn engine đó
+  if l:current_engine !~? 'BambooUs'
+    let g:ibus_prev_engine = l:current_engine
+  endif
+  " Khôi phục lại engine
+  execute 'silent !' . 'ibus engine ' . g:ibus_prev_engine
+endfunction
+augroup IBusHandler
+    autocmd CmdLineEnter [/?] call IBusOn()
+    autocmd CmdLineLeave [/?] call IBusOff()
+    autocmd InsertEnter * call IBusOn()
+    autocmd InsertLeave * call IBusOff()
+augroup END
+call IBusOff()
+]])
