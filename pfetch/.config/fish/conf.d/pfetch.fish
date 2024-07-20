a53175ad14c5ab91cb4ee5a82ca821e449f84e10
--global <--global>
2024-07-20 23:56:22 +0700

    fix pfetch

--- pfetch/.config/fish/conf.d/pfetch.fish
+++ pfetch/.config/fish/conf.d/pfetch.fish
@@ -1,4 +1,4 @@
 set -gx PF_INFO ascii title os kernel shell pkgs uptime memory
-set -gx PF_CUSTOM_LOGOS ~/.config/custom_logo
-set -gx PF_ASCII kitty
-set -gx PF_PAD3 5
+# set -gx PF_CUSTOM_LOGOS ~/.config/custom_logo
+# set -gx PF_ASCII kitty
+# set -gx PF_PAD3 5
