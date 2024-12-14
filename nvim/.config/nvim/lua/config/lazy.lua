cb7c176eb7706fab66ed02594465836bd82d5d91
taitapcode <hoangductai2007@gmail.com>
2024-12-14 15:20:18 +0700

    nvim: move formatter from prettier to biome

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -36,8 +36,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
 
     { import = 'lazyvim.plugins.extras.linting.eslint' },
-    { import = 'lazyvim.plugins.extras.formatting.prettier' },
-
+    { import = 'lazyvim.plugins.extras.formatting.biome' },
     -- import/override with your plugins
     { import = 'plugins' },
   },
