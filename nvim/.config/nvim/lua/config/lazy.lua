b1975dd47261b874ef5475c57be325c9be748938
taitapcode <hoangductai2007@gmail.com>
2025-01-01 22:23:12 +0700

    nvim: add svelte and nix lsp

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -34,6 +34,8 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.util.dot' },
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
+    { import = 'lazyvim.plugins.extras.lang.nix' },
+    { import = 'lazyvim.plugins.extras.lang.svelte' },
 
     { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.formatting.prettier' },
