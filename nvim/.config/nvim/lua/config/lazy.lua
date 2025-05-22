66cbc63f479338dce6e8b5dc2c73e255bddbf9a5
taitapcode <hoangductai2007@gmail.com>
2025-05-22 14:37:01 +0700

    nvim: add go lang

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ require('lazy').setup({
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.tailwind' },
     -- { import = 'lazyvim.plugins.extras.lang.nix' },
+    { import = 'lazyvim.plugins.extras.lang.go' },
     { import = 'lazyvim.plugins.extras.lang.svelte' },
 
     { import = 'lazyvim.plugins.extras.linting.eslint' },
