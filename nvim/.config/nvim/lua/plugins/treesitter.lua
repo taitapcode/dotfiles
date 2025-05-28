bb92131a22bbcf34bc1d426cf57246d5c660ad93
taitapcode <hoangductai2007@gmail.com>
2025-05-28 12:06:48 +0700

    feat: add treesitter support for HTML and Godot languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,6 +7,10 @@ return {
       vim.list_extend(opts.ensure_installed, {
         'css',
         'scss',
+        'html',
+        'gdscript',
+        'godot_resource',
+        'gdshader',
       })
     end
   end,
