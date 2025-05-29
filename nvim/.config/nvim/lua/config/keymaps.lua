8a704275db0784c2d9b720512cec65a6ddd4ce71
taitapcode <hoangductai2007@gmail.com>
2025-05-29 10:19:10 +0700

    fix: Add comment for keymaps.lua and update lazy-lock.json

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,14 +7,19 @@ local function optDesc(desc)
   }
 end
 
+-- Resize split windows with Alt + hjkl
 set('n', '<m-k>', ':resize -1<CR>', optDesc('Resize up'))
 set('n', '<m-j>', ':resize +1<CR>', optDesc('Resize down'))
 set('n', '<m-l>', ':vertical resize -1<CR>', optDesc('Resize left'))
 set('n', '<m-h>', ':vertical resize +1<CR>', optDesc('Resize right'))
 
-set('x', '<leader>p', [["_dP]])
+-- Disable auto save replaced text to clipboard when pasting text
+set('x', '<leader>p', [["_dP]], optDesc('Paste without replacing clipboard'))
+
+-- Disable yank on delete
 set('n', 'x', [["_x]])
 set('n', 'X', [["_X]])
+
 set('n', '<Leader>ha', 'ggVG', optDesc('Select all'))
 set('n', '<m-w>', function()
   Snacks.bufdelete()
