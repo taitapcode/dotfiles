e99a395f902d3583e5f90a6bb38ecf62e3b701d0
taitapcode <hoangductai2007@gmail.com>
2025-08-27 20:07:43 +0700

    chore: update lazy-lock.json and editor.lua

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -141,4 +141,14 @@ return {
       opts.scroll.enabled = false
     end,
   },
+  -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
+  {
+    'akinsho/bufferline.nvim',
+    init = function()
+      local bufline = require('catppuccin.groups.integrations.bufferline')
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
