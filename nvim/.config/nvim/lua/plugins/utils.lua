b740be8074e7e860edf8c61db9d1e90410eee93b
taitapcode <hoangductai2007@gmail.com>
2025-09-03 00:30:52 +0700

    refactor: move bufferline.nvim catppuccin theme workaround from editor.lua to utils.lua
    - Relocated the bufferline.nvim catppuccin theme workaround from plugins/editor.lua to plugins/utils.lua for improved organization and clarity.
    
    - Updated lazy-lock.json plugin lockfile due to plugin updates.
    
    - This workaround remains until https://github.com/LazyVim/LazyVim/pull/6354 is merged.
    
    Signed-off-by: taitapcode <hoangductai2007@gmail.com>

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -1,4 +1,15 @@
 return {
+  -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
+  {
+    'akinsho/bufferline.nvim',
+    init = function()
+      local bufline = require('catppuccin.groups.integrations.bufferline')
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
+
   {
     'LazyVim',
     opts = {
