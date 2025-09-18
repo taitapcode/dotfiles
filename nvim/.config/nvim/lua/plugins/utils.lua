332ceeedb856350e6f4612219171870189a7e055
taitapcode <hoangductai2007@gmail.com>
2025-09-18 07:22:55 +0700

    chore(lazyvim): major update, sync plugins and remove bufferline workaround

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -1,14 +1,4 @@
 return {
-  -- TODO: Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
-  {
-    'akinsho/bufferline.nvim',
-    init = function()
-      local bufline = require('catppuccin.groups.integrations.bufferline')
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
   {
     'LazyVim',
     opts = {
