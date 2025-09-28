36b2bb470f0e9710421e19ab8a0f09f8c0678d0a
taitapcode <hoangductai2007@gmail.com>
2025-09-28 12:57:49 +0700

    refactor(nvim): remove sidekick plugin
    The sidekick plugin was removed because it was not being used.
    The tab keymap was also updated to remove the sidekick next edit
    suggestion function.

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require('lazy').setup({
     { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
     -- import any extras modules here
     { import = 'lazyvim.plugins.extras.ai.copilot' },
-    { import = 'lazyvim.plugins.extras.ai.sidekick' },
 
     { import = 'lazyvim.plugins.extras.ui.mini-indentscope' },
     { import = 'lazyvim.plugins.extras.coding.mini-comment' },
--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -26,14 +26,7 @@ return {
 
       opts.keymap = {
         preset = 'enter',
-        ['<Tab>'] = {
-          'select_next',
-          'snippet_forward',
-          function() -- sidekick next edit suggestion
-            return require('sidekick').nes_jump_or_apply()
-          end,
-          'fallback',
-        },
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
         ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
       }
     end,
