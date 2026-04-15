f9e7d3d83baa1aea1d447622e17bcfa0ebf5f615
taitapcode <hoangductai2007@gmail.com>
2026-04-16 05:56:36 +0700

    chore: update neovim configuration and plugin dependencies
    - add library path to fish environment
    - update all lazy.nvim plugin versions
    - remove custom clipboard provider configuration

--- fish/.config/fish/conf.d/env.fish
+++ fish/.config/fish/conf.d/env.fish
@@ -1,4 +1,5 @@
 set -gx EDITOR nvim
 set -gx CPLUS_INCLUDE_PATH $HOME/.local/include $CPLUS_INCLUDE_PATH
+set -gx LIBRARY_PATH $HOME/.local/lib $LIBRARY_PATH
 
 fish_add_path $HOME/go/bin $HOME/.bun/bin $HOME/.cache/.bun/bin $HOME/.local/share/bin
