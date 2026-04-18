b444c5cf4517520c5f1084a2477dd4a55183ae20
taitapcode <hoangductai2007@gmail.com>
2026-04-18 17:22:17 +0700

    chore: disable formatter for cpp and restructure format_on_save configuration

--- nvim/.config/nvim/lua/plugin/formatter.lua
+++ nvim/.config/nvim/lua/plugin/formatter.lua
@@ -5,9 +5,15 @@ require('conform').setup({
     lua = { 'stylua' },
     python = { 'black' },
     fish = { 'fish_indent' },
+    cpp = {},
   },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_format = 'fallback',
-  },
+  format_on_save = function(bufnr)
+    -- Disable with a list of filetypes
+    local ignore_filetypes = { 'cpp' }
+    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+      return
+    end
+
+    return { timeout_ms = 500, lsp_fallback = true }
+  end,
 })
