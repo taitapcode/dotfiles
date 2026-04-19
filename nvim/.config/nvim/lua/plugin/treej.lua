9a13e9306eae9950356b279474e9b39053957082
taitapcode <hoangductai2007@gmail.com>
2026-04-19 14:38:05 +0700

    feat: configure treesj with custom keymaps and join length

--- nvim/.config/nvim/lua/plugin/treej.lua
+++ nvim/.config/nvim/lua/plugin/treej.lua
@@ -1,4 +1,7 @@
 vim.pack.add({ 'https://github.com/wansmer/treesj' })
-require('treesj').setup()
+require('treesj').setup({
+  use_default_keymaps = false,
+  max_join_length = 200,
+})
 
 map('n', '<leader>m', require('treesj').toggle)
