f33c9a2e6263101a6bd9e9587d6ab2600b8ab869
taitapcode <hoangductai2007@gmail.com>
2026-04-20 16:33:43 +0700

    chore: format lua with single quotes and update gitsigns

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,19 +1,19 @@
-local core_modules = { "options", 'functions',"keymaps", "autocmds", "cmds" }
+local core_modules = { 'options', 'functions', 'keymaps', 'autocmds', 'cmds' }
 for _, mod in ipairs(core_modules) do
-  local ok, err = pcall(require, "config." .. mod)
+  local ok, err = pcall(require, 'config.' .. mod)
   if not ok then
-    vim.notify("Error loading config." .. mod .. "\n" .. err, vim.log.levels.ERROR)
+    vim.notify('Error loading config.' .. mod .. '\n' .. err, vim.log.levels.ERROR)
   end
 end
 
-local plugin_dir = vim.fn.stdpath("config") .. "/lua/plugin"
+local plugin_dir = vim.fn.stdpath('config') .. '/lua/plugin'
 if vim.fn.isdirectory(plugin_dir) == 1 then
   for _, file in ipairs(vim.fn.readdir(plugin_dir)) do
-    if file:match("%.lua$") then
-      local mod_name = "plugin." .. file:gsub("%.lua$", "")
+    if file:match('%.lua$') then
+      local mod_name = 'plugin.' .. file:gsub('%.lua$', '')
       local ok, err = pcall(require, mod_name)
       if not ok then
-        vim.notify("Error in " .. file .. ":\n" .. err, vim.log.levels.ERROR)
+        vim.notify('Error in ' .. file .. ':\n' .. err, vim.log.levels.ERROR)
       end
     end
   end
