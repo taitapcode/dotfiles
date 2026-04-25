cf560852106b2c0afde5bbd1c0ad69397ea71176
taitapcode <hoangductai2007@gmail.com>
2026-04-25 19:49:16 +0700

    refactor: simplify tree-sitter configuration

--- nvim/.config/nvim/lua/plugin/treesitter.lua
+++ nvim/.config/nvim/lua/plugin/treesitter.lua
@@ -43,12 +43,7 @@ local parsersToInstall = vim
   end)
   :totable()
 require('nvim-treesitter').install(parsersToInstall)
-
 require('nvim-treesitter-textobjects').setup()
-require('nvim-treesitter').setup({
-  autotag = { enable = true },
-  folds = { enable = true },
-})
 
 vim.api.nvim_create_autocmd('PackChanged', {
   callback = function(e)
@@ -65,6 +60,5 @@ vim.api.nvim_create_autocmd('PackChanged', {
 vim.api.nvim_create_autocmd('FileType', {
   callback = function()
     pcall(vim.treesitter.start)
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
