aaa81f2e4b92b86b9727ba2156a8c5490495f188
taitapcode <hoangductai2007@gmail.com>
2026-05-05 12:00:17 +0700

    refactor: consolidate note functions and update tmux configuration

--- fish/.config/fish/functions/note.fish
+++ fish/.config/fish/functions/note.fish
@@ -1,5 +1,38 @@
-function note --description 'Create a new file in ~/Notes'
-    mkdir -p ~/Notes
-    set filename ~/Notes/note-(date +%d%m%Y)".md"
-    nvim $filename
+function note
+    # Thiết lập đường dẫn thư mục Notes
+    set -l note_dir ~/Documents/Notes
+
+    # Kiểm tra và tạo thư mục nếu chưa tồn tại
+    if not test -d $note_dir
+        mkdir -p $note_dir
+    end
+
+    # Xử lý các subcommand
+    switch "$argv[1]"
+        case folder
+            # Mở Neovim tại thư mục Notes và gọi Snacks picker
+            cd $note_dir
+            nvim -c "lua Snacks.picker.files()"
+
+        case new
+            if set -q argv[2]
+                # note new {name}
+                nvim $note_dir/$argv[2].md
+            else
+                # note new (format %S:%M:%H-%d%m%Y)
+                set -l timestamp (date "+%S%M%H%d%m%Y")
+                nvim $note_dir/$timestamp.md
+            end
+
+        case "*"
+            if test -n "$argv[1]"
+                # Trường hợp gõ 'note something' mà không có 'new'
+                # Có thể tùy biến để nó hiểu là tạo note mới với {name}
+                nvim $note_dir/$argv[1].md
+            else
+                # Lệnh 'note' mặc định: format %H%d%m%Y
+                set -l timestamp (date "+%H%d%m%Y")
+                nvim $note_dir/$timestamp.md
+            end
+    end
 end
--- fish/.config/fish/functions/notes.fish
+++
@@ -1,5 +0,0 @@
-function notes --description 'Go to ~/Notes and open Neovim with Snacks file finder'
-    mkdir -p ~/Notes
-    cd ~/Notes
-    nvim -c "lua Snacks.picker.files()"
-end
