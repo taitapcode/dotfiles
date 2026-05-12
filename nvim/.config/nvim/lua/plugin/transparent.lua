7a2fa77b6003d74f1927e7959a14f3aadd4102c7
taitapcode <hoangductai2007@gmail.com>
2026-05-12 18:06:55 +0700

    chore: remove transparency settings and transparent.nvim plugin
    - remove background opacity and blur from ghostty config
    - delete transparent.nvim plugin configuration
    - remove transparent.nvim from nvim lock file
    - remove tmux status bar transparency settings

--- nvim/.config/nvim/lua/plugin/transparent.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add({ 'https://github.com/xiyaowong/transparent.nvim' })
-
-vim.g.transparent_enabled = true
-require('transparent').clear_prefix('BufferLine')
-require('transparent').setup({
-  extra_groups = { 'TabLineFill' },
-})
