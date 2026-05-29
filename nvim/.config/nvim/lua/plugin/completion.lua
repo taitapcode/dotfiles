2decfc10ceba4213dfe4b430429fa3275de5bfce
taitapcode <hoangductai2007@gmail.com>
2026-05-29 14:56:51 +0700

    refactor: move diagnostic config from completion to lsp

--- nvim/.config/nvim/lua/plugin/completion.lua
+++ nvim/.config/nvim/lua/plugin/completion.lua
@@ -28,17 +28,3 @@ cmp.setup({
     ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
   },
 })
-
-vim.diagnostic.config({
-  -- virtual_text = {
-  --   spacing = 4,
-  --   prefix = '●', -- Or any icon like '󰅚'
-  --   severity = { min = vim.diagnostic.severity.WARN },
-  -- },
-
-  virtual_lines = { current_line = true },
-  underline = true,
-  signs = true,
-  update_in_insert = true, -- Set to true if you want live feedback as you type
-  severity_sort = true,
-})
--- nvim/.config/nvim/lua/plugin/lsp.lua
+++ nvim/.config/nvim/lua/plugin/lsp.lua
@@ -89,3 +89,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
     lsp_map('<leader>cd', vim.diagnostic.open_float, 'Line Diagnostics')
   end,
 })
+
+vim.diagnostic.config({
+  -- virtual_text = {
+  --   spacing = 4,
+  --   prefix = '●', -- Or any icon like '󰅚'
+  --   severity = { min = vim.diagnostic.severity.WARN },
+  -- },
+
+  virtual_lines = { current_line = true },
+  underline = true,
+  signs = true,
+  update_in_insert = true, -- Set to true if you want live feedback as you type
+  severity_sort = true,
+})
