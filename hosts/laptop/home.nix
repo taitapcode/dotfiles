a524eba909868e4adba01be1de60f1e550d1abc4
taitapcode <hoangductai2007@gmail.com>
2026-06-22 22:55:53 +0700

    feat: add zen browser config and gtk theme

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -14,6 +14,51 @@
   home.homeDirectory = "/home/tai";
   home.stateVersion = "26.05";
 
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.banana-cursor;
+    name = "Banana";
+    size = 28;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "catppuccin-mocha-lavender-standard";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "lavender" ];
+        size = "standard";
+        tweaks = [ "normal" ];
+        variant = "mocha";
+      };
+    };
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk3";
+  };
+
   modules.home = {
     niri.enable = true;
     git.enable = true;
--- modules/home-manager/zen-browser.nix
+++ modules/home-manager/zen-browser.nix
@@ -1,4 +1,5 @@
 { config, lib, pkgs, inputs, ... }:
+
 let
   cfg = config.modules.home.zen-browser;
 in
@@ -13,6 +14,74 @@ in
     programs.zen-browser = {
       enable = true;
       setAsDefaultBrowser = true;
+
+      policies = {
+        AutofillAddressEnabled = false;
+        AutofillCreditCardEnabled = false;
+        DisableAppUpdate = false;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+      };
+
+      profiles.default = {
+        mods = [
+          "a6335949-4465-4b71-926c-4a52d34bc9c0"
+          "f7c71d9a-bce2-420f-ae44-a64bd92975ab"
+        ];
+
+        containersForce = true;
+        containers = {
+          Personal = {
+            color = "purple";
+            icon = "fingerprint";
+            id = 1;
+          };
+          Study = {
+            color = "blue";
+            icon = "briefcase";
+            id = 2;
+          };
+        };
+
+        spacesForce = true;
+        spaces = {
+          "Daily" = {
+            id = "c6de089c-410d-4206-961d-ab11f988d40a";
+            position = 1000;
+            icon = "💻";
+	    container = 1;
+          };
+          "Study" = {
+            id = "cdd10fab-4fc5-494b-9041-325e5759195b";
+            position = 2000;
+            icon = "📚";
+	    container = 2;
+          };
+        };
+
+        pinsForce = true;
+        pinsForceAction = "remove";
+        pins = {
+          "GitHub" = {
+            id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
+            url = "https://github.com";
+            position = 101;
+            isEssential = true;
+            container = 1;
+          };
+        };
+      };
     };
   };
 }
