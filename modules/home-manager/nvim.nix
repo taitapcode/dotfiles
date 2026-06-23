b92f918489e0e77f32e04e3cf2bbacd2a24ee4f1
taitapcode <hoangductai2007@gmail.com>
2026-06-23 09:42:16 +0700

    feat: enhance neovim config with clipboard and editor defaults

--- modules/home-manager/nvim.nix
+++ modules/home-manager/nvim.nix
@@ -6,11 +6,21 @@ in
   options.modules.home.neovim.enable = lib.mkEnableOption "Enable Neovim configuration";
 
   config = lib.mkIf cfg.enable  {
+    home.packages = [ pkgs.wl-clipboard ];
+
     programs.neovim = {
       enable = true;
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
+      extraConfig = ''
+        set tabstop=2
+        set shiftwidth=2
+        set expandtab
+        set number
+        set relativenumber
+        set clipboard=unnamedplus
+      '';
     };
   };
 }
