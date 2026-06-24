46ddbf8d372ed9f9e4c725aed0b5457dad6030cc
taitapcode <hoangductai2007@gmail.com>
2026-06-24 13:50:20 +0700

    feat: enhance yazi config with plugins and keymaps

--- modules/home-manager/program/yazi.nix
+++ modules/home-manager/program/yazi.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -15,12 +16,68 @@ in
       enable = true;
 
       settings = {
-        manager = {
+        mgr = {
           show_hidden = true;
-          sort_by = "name";
-          sort_sensitive = false;
           sort_dir_first = true;
         };
+        preview.image_quality = 90;
+        plugin.prepend_fetchers = [
+          {
+            url = "*";
+            run = "git";
+            group = "git";
+
+          }
+          {
+            url = "*/";
+            run = "git";
+            group = "git";
+          }
+        ];
+
+      };
+
+      plugins = with pkgs.yaziPlugins; {
+        full-border = full-border;
+        git = git;
+        smart-enter = smart-enter;
+        smart-paste = smart-paste;
+        no-status = no-status;
+      };
+
+      initLua = ''
+        require("full-border"):setup()
+        require("git"):setup({ order = 1500 })
+        require("smart-enter"):setup { open_multi = true }
+        require("no-status"):setup()
+      '';
+
+      keymap = {
+        mgr.prepend_keymap = [
+          {
+            on = [ "l" ];
+            run = "plugin smart-enter";
+            desc = "Enter the child directory, or open the file";
+          }
+          {
+            on = [ "p" ];
+            run = "plugin smart-paste";
+            desc = "Paste into the hovered directory or CWD";
+          }
+          {
+            on = [ "d" ];
+            run = "remove --permanently";
+            desc = "Remove file";
+          }
+        ];
+
+        input.prepend_keymap = [
+          {
+            on = [ "<Esc>" ];
+            run = "close";
+            desc = "Cancel input";
+          }
+        ];
       };
     };
   };
