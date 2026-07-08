a49d26c9385add8e193886f5c871edfb480c86bf
taitapcode <hoangductai2007@gmail.com>
2026-07-08 18:09:33 +0700

    chore: add anki addons

--- modules/home-manager/app/anki.nix
+++ modules/home-manager/app/anki.nix
@@ -11,6 +11,22 @@ in
   options.modules.home.app.anki.enable = lib.mkEnableOption "Enable Anki";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ (pkgs.anki.withAddons (with pkgs.ankiAddons; [ review-heatmap ])) ];
+    home.packages = [
+      (pkgs.anki.withAddons (
+        with pkgs.ankiAddons;
+        [
+          (review-heatmap.withConfig {
+            config = {
+              color_theme = "ice";
+              display_mode = "continuous";
+              calendar_start_day = 1;
+              exclude_manual_reschedules = true;
+            };
+          })
+          advanced-browser
+          image-occlusion-enhanced
+        ]
+      ))
+    ];
   };
 }
