66a7884e714dc2a5110b3ab381122b686f124038
taitapcode <hoangductai2007@gmail.com>
2026-07-16 10:59:38 +0700

    fix: add space themes to zen browser config

--- modules/home-manager/app/zen-browser.nix
+++ modules/home-manager/app/zen-browser.nix
@@ -110,117 +110,146 @@ in
         };
 
         spacesForce = true;
+        pinsForce = true;
+        pinsForceAction = "remove";
         spaces = {
           "Daily" = {
             id = "c6de089c-410d-4206-961d-ab11f988d40a";
             position = 1000;
             icon = "💻";
             container = 1;
+            pins = {
+              "GitHub" = {
+                id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
+                url = "https://github.com";
+                position = 101;
+                isEssential = true;
+                container = 1;
+              };
+              "Gmail" = {
+                id = "40ca2d05-a429-4539-b8d9-2ae477c021a5";
+                url = "https://mail.google.com/mail/u/0/#inbox";
+                position = 102;
+                isEssential = true;
+                container = 1;
+              };
+              "Drive" = {
+                id = "f0b6401d-40b6-4f05-9eba-b07ce03eb938";
+                url = "https://drive.google.com/drive/my-drive";
+                position = 103;
+                isEssential = true;
+                container = 1;
+              };
+              "Youtube" = {
+                id = "4e3b255e-9c63-42a9-bf31-f5cc98a01450";
+                url = "https://www.youtube.com/";
+                position = 104;
+                isEssential = true;
+                container = 1;
+              };
+              "Gemini" = {
+                id = "1cd614cf-cb2a-4e9d-97ba-b42be17ca761";
+                url = "https://gemini.google.com/app";
+                position = 105;
+                isEssential = true;
+                container = 1;
+              };
+              "Claude" = {
+                id = "f41edcd1-b367-409f-ad80-506924daf97e";
+                url = "https://claude.ai/";
+                position = 106;
+                isEssential = true;
+                container = 1;
+              };
+              "Youtube Music" = {
+                id = "55b18b31-65f5-4ade-ac44-cf54a3ddf325";
+                url = "https://music.youtube.com/";
+                position = 107;
+                isEssential = true;
+                container = 1;
+              };
+              "Facebook" = {
+                id = "3757ac88-55f5-4466-8111-18b6458cde11";
+                url = "https://facebook.com";
+                position = 108;
+                isEssential = true;
+                container = 1;
+              };
+              "Stardew Valley" = {
+                id = "81781902-ac7e-432f-8f67-88f8bea8fc09";
+                url = "https://stardewvalleywiki.com/";
+                position = 109;
+                isEssential = true;
+                container = 1;
+              };
+            };
+            theme = {
+              type = "gradient";
+              colors = [
+                {
+                  red = 0;
+                  green = 0;
+                  blue = 0;
+                  algorithm = "floating";
+                  lightness = 0;
+                  type = "explicit-lightness"; # Fixed
+                }
+              ];
+              opacity = 0.5;
+              texture = 0.0;
+            };
           };
           "Study" = {
             id = "cdd10fab-4fc5-494b-9041-325e5759195b";
             position = 2000;
             icon = "📚";
             container = 2;
-          };
-        };
 
-        pinsForce = false;
-        pinsForceAction = "remove";
-        pins = {
-          # Container 1
-          "GitHub" = {
-            id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
-            url = "https://github.com";
-            position = 101;
-            isEssential = true;
-            container = 1;
-          };
-          "Gmail" = {
-            id = "40ca2d05-a429-4539-b8d9-2ae477c021a5";
-            url = "https://mail.google.com/mail/u/0/#inbox";
-            position = 102;
-            isEssential = true;
-            container = 1;
-          };
-          "Drive" = {
-            id = "f0b6401d-40b6-4f05-9eba-b07ce03eb938";
-            url = "https://drive.google.com/drive/my-drive";
-            position = 103;
-            isEssential = true;
-            container = 1;
-          };
-          "Youtube" = {
-            id = "4e3b255e-9c63-42a9-bf31-f5cc98a01450";
-            url = "https://www.youtube.com/";
-            position = 104;
-            isEssential = true;
-            container = 1;
-          };
-          "Gemini" = {
-            id = "1cd614cf-cb2a-4e9d-97ba-b42be17ca761";
-            url = "https://gemini.google.com/app";
-            position = 105;
-            isEssential = true;
-            container = 1;
-          };
-          "Claude" = {
-            id = "f41edcd1-b367-409f-ad80-506924daf97e";
-            url = "https://claude.ai/";
-            position = 106;
-            isEssential = true;
-            container = 1;
-          };
-          "Youtube Music" = {
-            id = "55b18b31-65f5-4ade-ac44-cf54a3ddf325";
-            url = "https://music.youtube.com/";
-            position = 107;
-            isEssential = true;
-            container = 1;
-          };
-          "Facebook" = {
-            id = "3757ac88-55f5-4466-8111-18b6458cde11";
-            url = "https://facebook.com";
-            position = 108;
-            isEssential = true;
-            container = 1;
-          };
-          "Stardew Valley" = {
-            id = "81781902-ac7e-432f-8f67-88f8bea8fc09";
-            url = "https://stardewvalleywiki.com/";
-            position = 109;
-            isEssential = true;
-            container = 1;
-          };
-
-          # Container 2
-          "MyBK" = {
-            id = "5af8ecd0-55f0-4084-96f5-eb4bc62a5d4e";
-            url = "https://mybk.hcmut.edu.vn/my/index.action";
-            position = 101;
-            isEssential = true;
-            container = 2;
-          };
-          "HCMUT Gmail" = {
-            id = "e4efd123-3226-4606-b948-430f2d2c1566";
-            url = "https://mail.google.com/mail/u/0/#inbox";
-            position = 102;
-            isEssential = true;
-            container = 2;
-          };
-          "HCMUT Drive" = {
-            id = "ab7d2102-3877-46c1-b8fc-582080ef90b2";
-            url = "https://drive.google.com/drive/my-drive";
-            position = 103;
-            isEssential = true;
-            container = 2;
-          };
-          "HCMUT Courseware" = {
-            id = "37b822f1-4070-4fd6-ae22-92221880368a";
-            url = "https://www.hcmut-courseware.org/";
-            position = 104;
-            isEssential = true;
-            container = 2;
+            pins = {
+              "MyBK" = {
+                id = "5af8ecd0-55f0-4084-96f5-eb4bc62a5d4e";
+                url = "https://mybk.hcmut.edu.vn/my/index.action";
+                position = 101;
+                isEssential = true;
+                container = 2;
+              };
+              "HCMUT Gmail" = {
+                id = "e4efd123-3226-4606-b948-430f2d2c1566";
+                url = "https://mail.google.com/mail/u/0/#inbox";
+                position = 102;
+                isEssential = true;
+                container = 2;
+              };
+              "HCMUT Drive" = {
+                id = "ab7d2102-3877-46c1-b8fc-582080ef90b2";
+                url = "https://drive.google.com/drive/my-drive";
+                position = 103;
+                isEssential = true;
+                container = 2;
+              };
+              "HCMUT Courseware" = {
+                id = "37b822f1-4070-4fd6-ae22-92221880368a";
+                url = "https://www.hcmut-courseware.org/";
+                position = 104;
+                isEssential = true;
+                container = 2;
+              };
+            };
+            theme = {
+              type = "gradient";
+              colors = [
+                {
+                  red = 0;
+                  green = 0;
+                  blue = 0;
+                  algorithm = "floating";
+                  lightness = 0;
+                  type = "explicit-lightness"; # Fixed
+                }
+              ];
+              opacity = 0.5;
+              texture = 0.0;
+            };
           };
         };
       };
