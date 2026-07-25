e6440b8877d97b6eca90713d325f5ccb21770460
taitapcode <hoangductai2007@gmail.com>
2026-07-25 14:33:37 +0700

    fix(sddm): configure cursor theme for wayland compositor

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -11,19 +11,20 @@ in
   options.modules.nixos.sddm.enable = lib.mkEnableOption "Enable SDDM Display Manager configuration";
 
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      banana-cursor
+    ];
+
     services.displayManager.sddm = {
       enable = true;
-      wayland.enable = true;
-
-      package = pkgs.kdePackages.sddm;
-      extraPackages = with pkgs; [
-        banana-cursor
-      ];
-
+      wayland = {
+        enable = true;
+        compositor = "kwin";
+      };
       settings = {
         Theme = {
           CursorTheme = "Banana";
-          CursorSize = 24;
+          CursorSize = 28;
         };
       };
     };
