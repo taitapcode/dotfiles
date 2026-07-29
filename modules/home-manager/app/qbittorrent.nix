d7cd420f51942bd613f69a0fb5331b89ade480cb
taitapcode <hoangductai2007@gmail.com>
2026-07-29 16:37:09 +0700

    chore: reformat qbittorrent module args and reorder noctalia window rules

--- modules/home-manager/app/qbittorrent.nix
+++ modules/home-manager/app/qbittorrent.nix
@@ -1,9 +1,14 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.modules.home.app.qbittorrent;
 in
 {
-  options.modules.home.app.qbittorrent.enable = lib.mkEnableOption "Enable qBittorrent configuration";
+  options.modules.home.app.qbittorrent.enable = lib.mkEnableOption "Enable qbittorrent";
 
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [ qbittorrent ];
